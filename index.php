169b33e6d3d36eb80337189bb902dc08232f0dc5
Zota0 <77154534+Zota0@users.noreply.github.com>
2023-11-16 20:12:05 +0100

    something

--- index.php
+++ index.php
@@ -1,18 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="default_css.css">
+    <link rel="stylesheet" href="styles.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> </title>
 </head>
 <body>
     <div id="navbar">
-        <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg" alt="logo" id="logo">
+        <img src="assets/logo.png" alt="logo" id="logo">
         <ul class="navlinks">
             <li class="navlink"><a href="#home" class="navlink-link active" id="">home</a></li>
             <li class="navlink"><a href="#products" class="navlink-link" id="">products</a></li>
             <li class="navlink"><a href="#contact" class="navlink-link" id="">contact</a></li>
         </ul>
+    </div>
+    <div id="content">
+        <section>
+            <p class="space">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, quisquam tempora sunt doloribus animi, repellat repellendus maxime sapiente aspernatur porro deleniti esse harum similique, itaque cumque dolores qui sequi quis in recusandae molestias velit aliquam! Rerum, deleniti praesentium provident voluptate, dolore inventore ullam labore similique neque maiores modi, architecto autem!
+
+            </p>
+        </section>
+        <section>
+            <p class="space">
+                Quia sapiente velit, eos praesentium ullam temporibus, dolor nostrum non repudiandae consequuntur, magnam harum blanditiis asperiores sed perspiciatis illum. Corrupti incidunt unde, soluta eius laudantium eligendi doloremque asperiores nisi iste esse commodi nobis eveniet ad suscipit exercitationem, quo repellat quas accusantium hic enim odit molestiae nostrum. Nesciunt at natus illum!
+            </p>
+        </section>
+        <section>
+            <p class="space">
+                Odio, eligendi reiciendis aspernatur, fugiat dignissimos sint rerum, tempora labore accusantium aliquam natus. Labore voluptate harum sit dolore reprehenderit ipsum amet veritatis alias, quam facilis dolorem. Mollitia earum cum distinctio dolorum repellendus veritatis amet facere, ipsa totam quod odit soluta autem libero voluptates fugit eveniet illum architecto dolor hic recusandae?
+            </p>
+        </section>
+        <section>
+            <p class="space">
+                Praesentium nostrum, obcaecati voluptatem excepturi numquam magnam dicta cumque, ea impedit dolore voluptate iusto dignissimos porro doloribus nihil, molestiae modi blanditiis debitis. Nemo aperiam, consequuntur id corrupti unde nihil quas fuga inventore laboriosam, illo minima magnam deserunt repudiandae officiis veniam? Laboriosam cupiditate autem excepturi voluptatem placeat ab quia rerum sequi.
+            </p>
+        </section>
+        <section>
+            <p class="space">
+                Labore eaque, officia, fuga a voluptatum, iste quas tempora harum dolorem sunt beatae ea quo laborum saepe temporibus enim sequi laudantium mollitia ad ipsum deserunt nihil. Nostrum numquam exercitationem, cumque consequuntur beatae architecto ipsa odio ut quae molestiae ullam sint laudantium. Culpa repellendus molestias odit distinctio perspiciatis suscipit sunt a!
+                
+            </p>
+        </section>
+        <section id="footer">
+                <div id="left_foot"></div>
+                <div id="right_foot"></div>
+        </section>
     </div>
 </body>
 </html>
